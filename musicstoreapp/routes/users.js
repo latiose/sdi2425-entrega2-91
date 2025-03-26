ee2a9df73a96aeb4bbacdc3576dc99d77322e487
Pablo <uo294197@uniovi.es>
2025-03-26 23:24:08 +0100

    SDI-2425-914-10.3-Mensajes y alertas.

--- musicstoreapp/routes/users.js
+++ musicstoreapp/routes/users.js
@@ -14,9 +14,13 @@ module.exports = function (app, usersRepository) {
     }
     //res.send('usuario registrado');
     usersRepository.insertUser(user).then(userId => {
-      res.redirect("/users/login");
+      res.redirect("/users/login" +
+          "?message=Nuevo usuario registrado"+
+          "&messageType=alert-info ");
     }).catch(error => {
-      res.send("Error al insertar el usuario");
+      res.redirect("/users/signup" +
+          "?message=Se ha producido un error al registrar al usuario"+
+          "&messageType=alert-danger ");
     });
   });
   app.get('/users/login', function (req, res) {
@@ -33,14 +37,18 @@ module.exports = function (app, usersRepository) {
     usersRepository.findUser(filter, options).then(user => {
       if (user == null) {
         req.session.user = null;
-        res.send("Usuario no identificado");
+        res.redirect("/users/login" +
+            "?message=Email o password incorrecto"+
+            "&messageType=alert-danger ");
       } else {
         req.session.user = user.email;
         res.redirect("/publications");
       }
     }).catch(error => {
       req.session.user = null;
-      res.send("Se ha producido un error al buscar el usuario: " + error);
+      res.redirect("/users/login" +
+          "?message=Se ha producido un error al buscar el usuario"+
+          "&messageType=alert-danger ");
     });
   });
   app.get('/users/logout', function (req, res) {
